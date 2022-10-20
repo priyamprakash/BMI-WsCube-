14f53e2b694e122341564f583720f453a5927c3f
Priyam Prakash <priyamprakash99@gmail.com>
2022-10-20 12:10:25 +0530

    custom toolbar

--- app/src/main/java/com/priyam/bmi/MainActivity.java
+++ app/src/main/java/com/priyam/bmi/MainActivity.java
@@ -1,8 +1,10 @@
 package com.priyam.bmi;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_toolbar);
+
+        TextView textView = getSupportActionBar().getCustomView().findViewById(R.id.toolbar_title);
+        textView.setText("Body Mass Index");
     }
 }
